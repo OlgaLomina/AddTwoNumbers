9253e8c573e057f17cc541b39de675d4ce33ab01
Olga Lomina <olgalomina@outlook.com>
2018-10-30 16:22:25 -0700

    Add project files.

---
+++ Practice List1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int x)
+    {
+        val = x;
+        next = null;
+    }
+    public ListNode()
+    { next = null; }
+}
+public class Solution
+{
+    public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        if (l1 == null)
+            return l2;
+        if (l2 == null)
+            return l1;
+
+        ListNode res = new ListNode(0);
+        ListNode curr = res;
+        int rem = 0;
+
+        while (l1 != null || l2 != null)
+        {
+            int sum1 = 0, sum2 = 0;
+            if (l1 != null)
+                sum1 = l1.val;
+            if (l2 != null)
+                sum2 = l2.val;
+
+            int sum = sum1 + sum2 + rem;
+            rem = sum / 10;
+
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (l1 != null)
+                l1 = l1.next;
+            if (l2 != null)
+                l2 = l2.next;
+        }
+        if (rem > 0)
+        {
+            //add ListNode
+            curr.next = new ListNode(rem);
+        }
+        return res.next;
+    }
+
+    public static void Main()
+    {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        //l2.next.next = new ListNode(4);
+
+        ListNode list = AddTwoNumbers(l1, l2);
+        while (list != null)
+        {
+            Console.WriteLine(list.val);
+            list = list.next;
+        }
+    }
+}
